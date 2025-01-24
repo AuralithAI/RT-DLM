1797cff685585261bbd33aaf135e511cbb3d1b25
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-01-24 12:52:01 -0800

    add constructor parameters for RTDLM Config. vocab size, embed dimension, max_sequence length.

--- config.py
+++ config.py
@@ -3,5 +3,13 @@
 ####################################################################################
 
 class RTDLMConfig:
-    def __init__(self):
-        pass
+
+    # Constructor for RTDLMConfig:
+    # Parameters:
+    #   vocab_size: int - Vocabulary size
+    #   d_model: int - Embedding dimension
+    #   max_seq_length: int - Maximum sequence length
+    def __init__(self, vocab_size: int, d_model: int, max_seq_length: int):
+        self.vocab_size = vocab_size  
+        self.d_model = d_model  
+        self.max_seq_length = max_seq_length
