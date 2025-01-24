32c5a7f1f86b25cad458ccda67a7ed95a0c31782
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-01-24 13:20:40 -0800

    * Update libraries into requirements. * Comments for RTFLM Config. * Map position ids with token_ids for combined embeddings.

--- model.py
+++ model.py
@@ -1,6 +1,26 @@
-class EmbeddingLayer():
-    def __init__(self):
-        pass
+import haiku as hk
+import jax.numpy as jnp
+from config import RTDLMConfig
 
-    def __call__(self, *args, **kwds):
-        pass
+class EmbeddingLayer():
+    """
+       Constructor for EmbeddingLayer   
+    """
+    def __init__(self, vocab_size: int, d_model: int, max_seq_length: int):
+        super().__init__()
+        self.token_embedding = hk.Embed(vocab_size, d_model)
+        self.position_embedding = hk.Embed(max_seq_length, d_model)
+
+    def __call__(self, token_ids: jnp.ndarray, seq_length: int):
+        """
+        Combine token and positional embeddings.
+        Parameters:
+            token_ids: jnp.ndarray - Token IDs
+            seq_length: int - Sequence length
+        Returns:
+            jnp.ndarray: Combined embeddings
+        """
+        position_ids = jnp.arange(seq_length)[None, :] 
+        token_embeds = self.token_embedding(token_ids)  
+        position_embeds = self.position_embedding(position_ids)  
+        return token_embeds + position_embeds  
--- test_example/test_embedding.py
+++ test_example/test_embedding.py
@@ -1,3 +1,24 @@
 ####################################################################################
 #                   Test Embedding Model - Using Example
 ####################################################################################
+import jax
+import haiku as hk
+import jax.numpy as jnp
+from model import EmbeddingLayer
+
+def test_embedding():
+    vocab_size = 32000
+    d_model = 512
+    max_seq_length = 128
+
+    embedding_layer = EmbeddingLayer(vocab_size, d_model, max_seq_length)
+    inputs = jnp.array([[1, 2, 3], [4, 5, 6]]) 
+    seq_length = inputs.shape[1]
+
+    model = hk.transform(lambda x: embedding_layer(x, seq_length))
+    params = model.init(jax.random.PRNGKey(42), inputs)
+    outputs = model.apply(params, None, inputs)
+
+    print("Embedding Output Shape:", outputs.shape)
+
+test_embedding()
