f45e4cae91963cfb6042a1b80a41eaae86a4ebe4
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-01-24 13:32:17 -0800

    Create a self attention class using hk.multi_head mechanism.

--- model.py
+++ model.py
@@ -2,6 +2,9 @@ import haiku as hk
 import jax.numpy as jnp
 from config import RTDLMConfig
 
+"""
+    EmbeddingLayer class is used to create embeddings for token and positional embeddings.
+"""
 class EmbeddingLayer():
     """
        Constructor for EmbeddingLayer   
@@ -24,3 +27,11 @@ class EmbeddingLayer():
         token_embeds = self.token_embedding(token_ids)  
         position_embeds = self.position_embedding(position_ids)  
         return token_embeds + position_embeds  
+    
+
+"""
+    SelfAttention class is used to create self-attention mechanism using hk.multihead_attention.
+"""
+class SelfAttention():
+    def __init__(self):
+        super().__init__()
