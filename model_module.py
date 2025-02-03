44d73c6aad6703e570af6b17935aafc488f434f2
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-02-02 22:49:39 -0800

    create rtdlm which handles the model block

---
+++ model_module.py
@@ -0,0 +1,26 @@
+from model_module import TransformerBlock, SelfAttention, EmbeddingLayer, MixtureOfExperts, to_device
+from train import TrainConfig
+import haiku as hk
+
+class RTDLMModel(hk.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embed = EmbeddingLayer(config.vocab_size, config.d_model, config.max_seq_length)
+        self.transformer_blocks = [TransformerBlock(config.d_model, config.num_heads) for _ in range(config.num_layers)]
+        self.moe_layer = MixtureOfExperts(config.d_model, config.moe_experts, config.moe_top_k, dropout_rate=0.1)
+        self.final_layer = hk.Linear(config.vocab_size, w_init=hk.initializers.TruncatedNormal(0.02)) 
+
+    def __call__(self, inputs, rng):
+        x = self.embed(inputs, seq_length=inputs.shape[1])
+        for block in self.transformer_blocks:
+            x = block(x)
+
+        x = self.moe_layer(x, rng, is_training=True)
+        x = self.final_layer(x)
+
+        return to_device(x)
+
+def forward_fn(inputs, rng):
+    config = TrainConfig()
+    model = RTDLMModel(config)
+    return model(inputs, rng)
