00613571a0cf32d7e503acd419f605684d1735e6
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-02-02 23:44:47 -0800

    split rng into moe_rng and final_rng, ensuring each operation gets a separate PRNG key

--- model_module.py
+++ model_module.py
@@ -9,16 +9,16 @@ class RTDLMModel(hk.Module):
         self.embed = EmbeddingLayer(config.vocab_size, config.d_model, config.max_seq_length)
         self.transformer_blocks = [TransformerBlock(config.d_model, config.num_heads) for _ in range(config.num_layers)]
         self.moe_layer = MixtureOfExperts(config.d_model, config.moe_experts, config.moe_top_k, dropout_rate=0.1)
-        self.final_layer = hk.Linear(config.vocab_size, w_init=hk.initializers.TruncatedNormal(0.02)) 
 
     def __call__(self, inputs, rng):
-        rng, moe_rng = jrandom.split(rng)
+        rng, moe_rng, final_rng = jrandom.split(rng, 3)
         x = self.embed(inputs, seq_length=inputs.shape[1])
         for block in self.transformer_blocks:
             x = block(x)
 
         x = self.moe_layer(x, moe_rng, is_training=True)
-        x = self.final_layer(x)
+        final_layer = hk.Linear(TrainConfig().vocab_size, w_init=hk.initializers.TruncatedNormal(0.02, final_rng))
+        x = final_layer(x)
 
         return to_device(x)
 
