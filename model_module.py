e7c8673f7420d3a9125456c08cfa0a29e2b0700f
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-02-03 00:18:05 -0800

    Replaced jax.vmap with hk.vmap for Haiku-managed transformations

--- model_module.py
+++ model_module.py
@@ -8,21 +8,22 @@ class RTDLMModel(hk.Module):
         self.embed = EmbeddingLayer(config.vocab_size, config.d_model, config.max_seq_length)
         self.transformer_blocks = [TransformerBlock(config.d_model, config.num_heads) for _ in range(config.num_layers)]
         self.moe_layer = MixtureOfExperts(config.d_model, config.moe_experts, config.moe_top_k, dropout_rate=0.1)
-        self.final_layer = hk.Linear(config.vocab_size, w_init=hk.initializers.TruncatedNormal(0.02))  
-        
+        self.final_layer = hk.Linear(config.vocab_size, w_init=hk.initializers.TruncatedNormal(0.02))
+
     def __call__(self, inputs):
         x = self.embed(inputs, seq_length=inputs.shape[1])
 
         for block in self.transformer_blocks:
             x = block(x)
 
-        x = self.moe_layer(x, hk.next_rng_key(), is_training=True)  
-        x = self.final_layer(x) 
+        x = self.moe_layer(x, is_training=True)
+
+        x = self.final_layer(x)
 
         return to_device(x)
 
+config = TrainConfig()
+model = RTDLMModel(config) 
 
 def forward_fn(inputs):
-    config = TrainConfig()
-    model = RTDLMModel(config)
     return model(inputs)
