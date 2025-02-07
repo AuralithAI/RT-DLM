834562987e7be2213d8661e4070450189d96b92b
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-02-07 00:05:21 -0800

    config change.

--- train_config.py
+++ train_config.py
@@ -6,8 +6,8 @@ class TrainConfig:
         self.num_layers = 2
         self.moe_experts = 2
         self.moe_top_k = 1
-        self.max_seq_length = 64
-        self.batch_size = 1
-        self.learning_rate = 3e-5
+        self.max_seq_length = 128
+        self.batch_size = 4
+        self.learning_rate = 1e-5
         self.num_epochs = 3
         self.eval_interval = 50
