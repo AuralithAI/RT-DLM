196a7a0ee1df907f6da8b1851a3f0aab36141426
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-02-07 00:06:37 -0800

    reduce batch size.

--- train_config.py
+++ train_config.py
@@ -7,7 +7,7 @@ class TrainConfig:
         self.moe_experts = 2
         self.moe_top_k = 1
         self.max_seq_length = 64
-        self.batch_size = 4
+        self.batch_size = 1
         self.learning_rate = 1e-5
         self.num_epochs = 3
         self.eval_interval = 50
