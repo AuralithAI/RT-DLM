7ce5d1f43064c16b19b5dc82da6815eb95250c2e
Ashutosh Kumar <kumar.ashutosh832@gmail.com>
2025-02-10 01:38:21 +0000

    fix numpy and pandas integration versions

--- lambda-env.sh
+++ lambda-env.sh
@@ -20,6 +20,20 @@ install_pip_and_jax() {
     pip3 install --upgrade "jax[cuda12]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 }
 
+# Function to install Tkinter and GUI dependencies
+install_tkinter() {
+    echo "Installing Tkinter and GUI dependencies..."
+    if [[ "$OS_NAME" == "ubuntu" || "$OS_NAME" == "debian" ]]; then
+        sudo apt update -y
+        sudo apt install -y python3-tk python3.10-tk tk-dev
+        sudo apt install -y libxrender1 libxext6 libsm6 libxft2
+        sudo apt update -y
+        sudo apt install -y python3-tk
+    else
+        echo "Unsupported OS for Tkinter installation"
+    fi
+}
+
 # Function to install project dependencies
 install_requirements() {
     if [ -f "requirements.txt" ]; then
@@ -30,9 +44,19 @@ install_requirements() {
     fi
 }
 
+#Degrade numpy and pandas to match lambda instance
+downgrade_numpy() {
+    echo "Degrade numpy to less than version 2.xx"
+    pip3 uninstall -y numpy pandas datasets
+    pip3 install --force-reinstall -y numpy pandas datasets
+
+}
+
 # Run functions
 install_pip_and_jax
+install_tkinter
 install_requirements
+downgrade_numpy
 
 # Verify installations
 pip3 --version
