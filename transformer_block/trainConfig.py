99715aabc694274f6584b88f68b0ccfcdc7d796d
Ashutosh Kumar <kumar.ashutosh832@gmail.com>
2025-02-15 08:51:56 +0000

    Training loss for transformer block

--- transformer_block/trainConfig.py
+++ transformer_block/trainConfig.py
@@ -1,13 +1,13 @@
 class TrainConfig:
     def __init__(self):
         self.vocab_size = 6145
-        self.d_model = 96
+        self.d_model = 128
         self.num_heads = 4
-        self.num_layers = 6
+        self.num_layers = 8
         self.moe_experts = 8
         self.moe_top_k = 2
         self.max_seq_length = 64
-        self.batch_size = 32
+        self.batch_size = 64
         self.learning_rate = 2e-4
         self.num_epochs = 150
         self.eval_interval = 50
