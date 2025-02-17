1398867b21bceba6d749e7e81346ee53984106a5
Ashutosh Kumar <kumar.ashutosh832@gmail.com>
2025-02-17 01:26:43 +0000

    Training config for self-attention with transformer

--- train_config.py
+++ train_config.py
@@ -1,13 +1,13 @@
 class TrainConfig:
     def __init__(self):
         self.vocab_size = 6145
-        self.d_model = 512
-        self.num_heads = 12
-        self.num_layers = 16
+        self.d_model = 256
+        self.num_heads = 8
+        self.num_layers = 12
         self.moe_experts = 8
         self.moe_top_k = 2
         self.max_seq_length = 64
-        self.batch_size = 64
+        self.batch_size = 32
         self.learning_rate = 2e-4
         self.num_epochs = 150
         self.eval_interval = 50
