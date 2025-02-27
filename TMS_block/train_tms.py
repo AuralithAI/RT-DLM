7c7d4a84d9167ddefd7f0b1982b53caa8951e185
Ashutosh Kumar <ashutosh.ak.kumar@siemens.com>
2025-02-26 16:37:41 -0800

    pass additional optional arguments for threshold and epsilon

--- TMS_block/train_tms.py
+++ TMS_block/train_tms.py
@@ -47,7 +47,7 @@ def train_and_evaluate(config, losses, similarity_scores, thought_logs):
     logger.info(f"Dataset loaded - {inputs.shape[0]} samples")
 
     # Model definition
-    def forward_fn(inputs, return_attention=False, retrieved_memory_ltm=None, retrieved_memory_stm=None, retrieved_memory_mtm=None):
+    def forward_fn(inputs, return_attention=False, retrieved_memory_ltm=None, retrieved_memory_stm=None, retrieved_memory_mtm=None, **kwargs):
         model = TMSModel(
             d_model=config.d_model, 
             num_heads=config.num_heads, 
@@ -63,7 +63,7 @@ def train_and_evaluate(config, losses, similarity_scores, thought_logs):
             mtm_weight=config.mtm_weight
         )
         return model(inputs, return_attention=return_attention, retrieved_memory_ltm=retrieved_memory_ltm, 
-                     retrieved_memory_stm=retrieved_memory_stm, retrieved_memory_mtm=retrieved_memory_mtm)
+                     retrieved_memory_stm=retrieved_memory_stm, retrieved_memory_mtm=retrieved_memory_mtm, **kwargs)
 
     logger.info("Transforming model with Haiku")
     model = hk.transform_with_state(forward_fn)
